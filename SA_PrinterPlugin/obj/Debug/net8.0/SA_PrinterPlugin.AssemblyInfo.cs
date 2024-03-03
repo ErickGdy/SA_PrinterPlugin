5198a00df1c7f66385a86c07ce434623e5df6955
DESKTOP-5C051FI\Erick VM <egodoy@superadministrador.com.mx>
2024-03-02 19:11:18 -0700

    ª

--- SA_PrinterPlugin/obj/Debug/net8.0/SA_PrinterPlugin.AssemblyInfo.cs
+++ SA_PrinterPlugin/obj/Debug/net8.0/SA_PrinterPlugin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SA_PrinterPlugin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2c14a36c135c73cc3dbccc7798962f555afa8e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e21c51ca283b15b33002fcda464a92c8f3e2e8bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SA_PrinterPlugin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SA_PrinterPlugin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
